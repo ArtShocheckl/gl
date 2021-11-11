c92fd3c26d016c61597481c195683b356cba24e5
ArtShocheckl <92567290+ArtShocheckl@users.noreply.github.com>
2021-11-11 09:53:43 +0300

    Update itc_print_copy_str.cpp

--- itc_print_copy_str.cpp
+++ itc_print_copy_str.cpp
@@ -1,10 +1,17 @@
 
+
 #include "str_easy.h"
 
 void itc_print_copy_str(string str, int number)
 {
+    int i = 0;
+    int g = -1;
     if( number < 0 )
-        return -1;
-	for (int i = 0; i < number; i++)
-		cout << str << endl;
+        cout<< g;
+    if(number >= 0){
+        while( i < number){
+            cout << str;
+            number++;
+        }
+    }
 }
