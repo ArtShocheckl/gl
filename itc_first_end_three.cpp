42aec1d172436b7e50de0f8c7ea984e7a627b277
ArtShocheckl <92567290+ArtShocheckl@users.noreply.github.com>
2021-11-11 10:39:39 +0300

    Update itc_first_end_three.cpp

--- itc_first_end_three.cpp
+++ itc_first_end_three.cpp
@@ -7,7 +7,7 @@ void itc_first_end_three(string str)
 	if (len <= 5)
 	{
 		for (int i = 0; i < len; i++)
-			cout << str[0] << endl;
+			cout << str[0];
 	}
 	else 
 		cout << str[0] << str[1] << str[2] << str[len - 3] << str[len - 2] << str[len - 1];
