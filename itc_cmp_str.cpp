c59482bfa50e44c29f258970de21c98ff82dd755
ArtShocheckl <92567290+ArtShocheckl@users.noreply.github.com>
2021-11-11 09:39:10 +0300

    Update itc_cmp_str.cpp

--- itc_cmp_str.cpp
+++ itc_cmp_str.cpp
@@ -4,9 +4,6 @@ string itc_cmp_str(string str1, string str2, int num)
 {
 	long long len = itc_len(str1);
 	string new_str;
-	if (num<0)
-        return "";
-        //return str1 + str2;
 	if (num >= len)
 		return str1 + str2;
 	for (long long i = 0; i < len; i++)
